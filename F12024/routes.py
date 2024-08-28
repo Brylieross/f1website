0d7feed0c7150d88cb5382945a2e00e7bcb463ff
Brylieross <140353043+Brylieross@users.noreply.github.com>
2024-08-28 14:05:51 +1200

    commit 3

--- F12024/routes.py
+++ F12024/routes.py
@@ -69,5 +69,10 @@ def search():
     return render_template("search.html", results=results)
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
 if __name__ == "__main__":
     app.run(debug=True)
